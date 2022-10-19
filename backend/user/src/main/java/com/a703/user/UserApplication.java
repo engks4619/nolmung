c24da6237250dc13b8128c84a0a3a0097b411cdf
minju0919 <minju0919@naver.com>
2022-10-19 10:39:28 +0900

    chore: javafx 라이브러리 삭제

--- backend/user/src/main/java/com/a703/user/UserApplication.java
+++ backend/user/src/main/java/com/a703/user/UserApplication.java
@@ -1,6 +1,5 @@
 package com.a703.user;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
