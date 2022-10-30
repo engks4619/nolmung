978d2780f72e2aeb3fd419632c3b5a08bbee4b3e
ryan8753 <ckdgus8753@gmail.com>
2022-10-31 04:57:30 +0900

    feat: 푸터기능구현

---
+++ frontend/src/pages/Spots.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Spots() {
+  return (
+    <View>
+      <Text>스팟 추천</Text>
+    </View>
+  );
+}
+
+export default Spots;
