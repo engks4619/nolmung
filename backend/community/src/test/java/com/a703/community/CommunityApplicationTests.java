a71e4af8e6f7a7e3e11deaaa1f6b8006dc616177
thwjd793@naver.com <thwjd793@naver.com>
2022-11-02 13:23:15 +0900

    fix: test코드수정

--- backend/community/src/test/java/com/a703/community/CommunityApplicationTests.java
+++ backend/community/src/test/java/com/a703/community/CommunityApplicationTests.java
@@ -1,21 +1,19 @@
 package com.a703.community;
 
-import com.a703.community.util.WeatherUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
-@SpringBootTest
+@SpringBootTest(classes = CommunityApplication.class)
 class CommunityApplicationTests {
 
-	@Autowired
-	WeatherUtil weatherUtil;
+//	@Autowired
+//	WeatherUtil weatherUtil;
 
 	@Test
 	void contextLoads() throws IOException {
-		weatherUtil.lookUpWeather();
+//		weatherUtil.lookUpWeather();
 	}
 
 }
