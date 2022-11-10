11385eebaea2dd54b34cb2609d9724e91e517624
engks4619 <engks4619@naver.com>
2022-11-10 23:36:25 +0900

    design: 게시글 작성 페이지 UI

--- frontend/src/pages/RegistArticle.tsx
+++ frontend/src/pages/RegistArticle.tsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
 import Home from '@assets/home.svg';
 import Paw from '@assets/paw.svg';
@@ -23,57 +30,61 @@ const RegistArticle = () => {
   const [dog, setDog] = useState<string>('');
   return (
     <View style={styles.container}>
-      <View style={styles.hContainer}>
-        <Text style={styles.title}>게시글 작성</Text>
-        <Pressable>
-          <Text style={styles.brown}>등록</Text>
-        </Pressable>
-      </View>
-      <SelectDropdown
-        data={categroyTypes}
-        buttonStyle={styles.dropdownBtnStyle}
-        buttonTextStyle={
-          category === '' ? styles.txtStyleNone : styles.txtStyle
-        }
-        renderDropdownIcon={isOpened => {
-          return dropdownIcon(isOpened);
-        }}
-        defaultButtonText={defaultCategoryMsg}
-        dropdownStyle={styles.dropDownStyle}
-        rowStyle={styles.rowStyle}
-        rowTextStyle={styles.rowTextStyle}
-        onSelect={(selectedItem, idx) => {
-          setCategory(selectedItem);
-        }}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          return item;
-        }}
-      />
-      <TextInput placeholder="제목을 입력해주세요" style={styles.textInput} />
-      <SelectDropdown
-        data={dogList}
-        buttonStyle={styles.dropdownBtnStyle}
-        buttonTextStyle={dog === '' ? styles.txtStyleNone : styles.txtStyle}
-        renderDropdownIcon={isOpened => {
-          return dropdownIcon(isOpened);
-        }}
-        defaultButtonText={defaultDogMsg}
-        dropdownStyle={styles.dropDownStyle}
-        rowStyle={styles.rowStyle}
-        rowTextStyle={styles.rowTextStyle}
-        onSelect={(selectedItem, idx) => {
-          setDog(selectedItem);
-        }}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          return item;
-        }}
-      />
+      <ScrollView>
+        <View style={styles.hContainer}>
+          <Text style={styles.title}>게시글 작성</Text>
+          <View style={styles.registBtn}>
+            <Pressable>
+              <Text style={styles.brown}>등록</Text>
+            </Pressable>
+          </View>
+        </View>
+        <SelectDropdown
+          data={categroyTypes}
+          buttonStyle={styles.dropdownBtnStyle}
+          buttonTextStyle={
+            category === '' ? styles.txtStyleNone : styles.txtStyle
+          }
+          renderDropdownIcon={isOpened => {
+            return dropdownIcon(isOpened);
+          }}
+          defaultButtonText={defaultCategoryMsg}
+          dropdownStyle={styles.dropDownStyle}
+          rowStyle={styles.rowStyle}
+          rowTextStyle={styles.rowTextStyle}
+          onSelect={(selectedItem, idx) => {
+            setCategory(selectedItem);
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem;
+          }}
+          rowTextForSelection={(item, index) => {
+            return item;
+          }}
+        />
+        <TextInput placeholder="제목을 입력해주세요" style={styles.textInput} />
+        <SelectDropdown
+          data={dogList}
+          buttonStyle={styles.dropdownBtnStyle}
+          buttonTextStyle={dog === '' ? styles.txtStyleNone : styles.txtStyle}
+          renderDropdownIcon={isOpened => {
+            return dropdownIcon(isOpened);
+          }}
+          defaultButtonText={defaultDogMsg}
+          dropdownStyle={styles.dropDownStyle}
+          rowStyle={styles.rowStyle}
+          rowTextStyle={styles.rowTextStyle}
+          onSelect={(selectedItem, idx) => {
+            setDog(selectedItem);
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem;
+          }}
+          rowTextForSelection={(item, index) => {
+            return item;
+          }}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -81,37 +92,40 @@ const RegistArticle = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    alignItems: 'center',
   },
   hContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-    fontSize: 15,
+    fontSize: 12,
+    fontWeight: '700',
     color: 'black',
+    paddingVertical: 5,
+  },
+  registBtn: {
+    position: 'absolute',
+    right: 0,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
     paddingVertical: 10,
   },
   brown: {
-    fontSize: 12,
-    textAlign: 'right',
+    fontSize: 10,
     color: MAIN_COLOR,
     fontWeight: '700',
   },
-  dropdown: {
-    width: '100%',
-    backgroundColor: 'yellow',
-  },
+
   dropdownBtnStyle: {
     width: '100%',
-    backgroundColor: 'white',
+    backgroundColor: 'yellow',
   },
   txtStyleNone: {
     color: 'gray',
     textAlign: 'left',
     paddingLeft: 10,
-    fontSize: 12,
+    fontSize: 10,
   },
   txtStyle: {
     color: 'black',
@@ -127,12 +141,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#EFEFEF',
   },
   rowTextStyle: {
-    fontSize: 12,
+    fontSize: 10,
   },
   textInput: {
     width: '100%',
     paddingHorizontal: 25,
-    fontSize: 12,
+    fontSize: 10,
   },
 });
 
