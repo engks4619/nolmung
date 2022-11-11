0182b53b510edad87dcb23461605d8f06c9e9d9a
minju0919 <minju0919@naver.com>
2022-11-11 14:57:06 +0900

    feat: 채팅방 생성 이벤트

--- backend/chat/src/app.js
+++ backend/chat/src/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const SocketIO = require('socket.io');
-// const WebSocket = require('ws');
 
-const indexRouter = require('../routes');
+// const indexRouter = require('../routes');
 const connect = require('../schemas');
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
 
 const app = express();
 const httpServer = http.createServer(app); // express http 서버 생성
@@ -15,35 +16,15 @@ const io = SocketIO(httpServer, {
 		methods: ['GET', 'POST'],
 	},
 });
-// const wss = new WebSocket.Server({ server });
 
 app.use(cors()); // cors를 미들웨어로 사용하도록 등록
-app.use(indexRouter);
+// app.use(indexRouter);
 const PORT = process.env.PORT || 5000;
 
 connect();
 
-// wss.on('connection', (ws, req) => { // 웹소켓 연결 시
-// 	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-// 	console.log('새로운 클라이언트 접속', ip);
-
-// 	ws.on('welcome', (msg) => {
-// 		console.log(msg);
-// 		ws.emit('reply', '서버에서 응답');
-// 	});
-
-// 	ws.on('message', (message) => { // 클라이언트로부터 메시지
-// 		console.log(message.toString());
-// 	});
-// 	ws.on('error', (error) => { // 에러 시
-// 		console.error(error);
-// 	});
-// 	ws.on('close', () => { // 연결 종료 시
-// 		console.log('클라이언트 접속 해제', ip);
-// 		clearInterval(ws.interval);
-// 	});
-
-// });
+const room = io.of('/room');
+const chat = io.of('/chat');
 
 // 소켓 연결 및 이벤트
 io.on('connection', (socket) => {
@@ -61,8 +42,39 @@ io.on('connection', (socket) => {
 	});
 });
 
+room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
+
+    const req = socket.request;
+
+	// 채팅방 생성 후 join
+    socket.on('newRoom', async(data) => {
+      console.log('newRoom 이벤트 발생');
+
+      try {
+        const newRoom = await Room.create({
+          ownerIdx: data.ownerIdx,
+          postIdx: data.postIdx,
+        });
+
+        console.log("DB 저장 완료");
+		console.log('채팅방 생성되었습니다.');
+        console.log("newRoom 아이디 : " + newRoom._id);  
+
+		socket.join(newRoom._id);
+
+        socket.emit('newRoomId', newRoom._id);  
+        
+      } catch (error) {
+        console.error(error);
+      }
+    })
+
+	socket.on('disconnect', () => {
+		console.log('room 네임스페이스 접속 해제');
+	});
+});
+
 httpServer.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
 });
-
-// webSocket(server, app);
