5ff50a58f60ea438ef9816a1cebf77c2d4e1ba1d
minjuukim <kminju919@gmail.com>
2022-11-12 13:04:24 +0900

    feat: 채팅방 목록 및 채팅내역 조회

--- backend/chat/src/app.js
+++ backend/chat/src/app.js
@@ -27,15 +27,11 @@ const room = io.of('/room');
 const chat = io.of('/chat');
 
 var login_ids = {};   // 로그인 id 매핑 (로그인 ID -> 소켓 ID)
+var roomId = "";
 
 // 소켓 연결 및 이벤트
 io.on('connection', (socket) => {
 	console.log('소켓 연결 완료! socket 아이디 : ', socket.id);
-
-	socket.on('welcome', (msg) => {
-		console.log(msg);
-		socket.emit('reply', '서버에서 응답');
-	});
 
 	// 'login' 이벤트를 받았을 때의 처리
     socket.on('login', function(login) {
@@ -67,18 +63,28 @@ room.on('connection', (socket) => {
       console.log('newRoom 이벤트 발생');
 
       try {
-        const newRoom = await Room.create({
-          ownerIdx: data.ownerIdx,
-          postIdx: data.postIdx,
-        });
 
-        console.log("DB 저장 완료");
-		console.log('채팅방 생성되었습니다.');
-        console.log("newRoom 아이디 : " + newRoom._id);  
+        const room = await Room.findOne({ ownerIdx: data.ownerIdx, postIdx: data.postIdx });
 
-		socket.join(newRoom._id);
-
-        socket.emit('newRoomId', newRoom._id);  
+        if (room == null) {
+          console.log("해당 방이 없습니다. ");
+          const newRoom = await Room.create({
+            ownerIdx: data.ownerIdx,
+            postIdx: data.postIdx,
+          });
+  
+          roomId = newRoom._id;
+          console.log("DB 저장 완료");
+          console.log('채팅방 생성되었습니다.');
+          console.log("newRoom 아이디 : " + newRoom._id);  
+  
+          socket.join(newRoom._id);
+  
+          socket.emit('newRoomId', newRoom._id);  
+        } else {
+          console.log('해당 채팅방이 이미 존재합니다. roomId: ', room._id);
+          socket.emit('newRoomId', room._id);
+        }
         
       } catch (error) {
         console.error(error);
@@ -89,6 +95,46 @@ room.on('connection', (socket) => {
 		console.log('room 네임스페이스 접속 해제');
 	});
 });
+
+chat.on('connection', (socket) => {
+  console.log('chat 네임스페이스에 접속');
+
+  // 채팅방 입장
+  socket.on('join', async(roomId) => {
+    socket.join(roomId);
+    console.log(roomId + ' 채팅방에 입장했습니다.');
+
+    try {
+      const chats = await Chat.find({ room: roomId }).sort('createdAt');
+      socket.emit('chats', chats);
+    } catch (error) {
+      console.log(error);
+    }
+
+  });
+
+  // DB 저장 후 방에 뿌려줌.
+  socket.on('messageS', async(data) => {    // 클라이언트로부터 메시지 수신
+    
+    try {
+      const chatInfo = await Chat.create({
+        room: data.roomId,
+        user: data.sender,
+        chat: data.chat
+      });
+      
+      console.log(data.chat);
+      console.log('클라이언트로부터 message 이벤트를 받았습니다.');
+  
+      chat.to(data.roomId).emit('messageC', data);
+
+    } catch (error) {
+      console.error(error);
+    }
+
+  });
+
+})
 
 httpServer.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
