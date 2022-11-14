7fbc73a772a164ddb579feb7b15745b03a442ee4
김창현 <ckdgus8753@gmail.com>
2022-11-14 18:00:25 +0900

    feat:스크린전송구현중

--- frontend/src/templates/LogviewTemplate.tsx
+++ frontend/src/templates/LogviewTemplate.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
 import NaverMapView, {Marker, Polyline, Coord} from 'react-native-nmap';
 import {DetailDogProps} from '@molecules/DetailDog';
 import DetailDogs from '@organisms/DetailDogs';
 import {MAIN_COLOR} from '~/const';
+import ViewShot from 'react-native-view-shot';
 
 interface Props {
   functions: Array<Function>;
@@ -12,7 +13,10 @@ interface Props {
   isOver: boolean;
   myPosition: Coord;
 }
-
+interface Photo {
+  name: string;
+  uri: any;
+}
 function LogViewTemplate({
   functions,
   path,
@@ -20,39 +24,110 @@ function LogViewTemplate({
   isOver,
   myPosition,
 }: Props) {
-  console.log('myposition', myPosition);
+  const [photo, setPhoto] = useState<Photo | null>(null);
+  const ref: any = useRef();
+  const createFormData = (image: any, body: any = {}) => {
+    const data = new FormData();
+
+    data.append('photo', {
+      name: image.name,
+      uri: image.uri.replace('file://', ''),
+    });
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+
+    return data;
+  };
+  const captureMap = () => {
+    ref.current.capture().then((uri: any) => {
+      console.log('uri', uri);
+      setPhoto({
+        name: 'imageName',
+        uri: uri,
+      });
+    });
+  };
+  const sendDatas = () => {
+    const b = createFormData(photo, {
+      ownerIdx: -1650769681,
+      walkerIdx: -1650769681,
+      distance: 100.0,
+      time: 5,
+      startDate: '2022.10.26 00:00:00',
+      endDate: '2022.10.26 00:00:00',
+      walkedDogList: [1, 2, 3],
+      latitudes: [1.1, 2.1],
+      longitudes: [2.4, 3.4],
+    });
+    fetch('http://nolmung.kr/api/withdog/walk', {
+      method: 'POST',
+      body: b,
+      // body: createFormData(photo, {
+      //   ownerIdx: -1650769681,
+      //   walkerIdx: -1650769681,
+      //   distance: 100.0,
+      //   time: 5,
+      //   startDate: '2022.10.26 00:00:00',
+      //   endDate: '2022.10.26 00:00:00',
+      //   walkedDogList: [1, 2, 3],
+      //   latitudes: [1.1, 2.1],
+      //   longitudes: [2.4, 3.4],
+      // }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log('response', response);
+      })
+      .catch(error => {
+        console.log('formData', b);
+        console.log('error1', error);
+      });
+  };
   if (isOver) {
     return (
       <View style={styles.logViewContainer}>
+        <Pressable onPress={() => captureMap()}>
+          <Text>캡쳐하기</Text>
+        </Pressable>
+        <Pressable onPress={() => sendDatas()}>
+          <Text>사진전송</Text>
+        </Pressable>
         <DetailDogs dogInfoList={dogInfoList} />
-        <View style={styles.mapViewContainer}>
-          <NaverMapView
-            style={styles.nmap}
-            zoomControl={true}
-            center={{
-              zoom: 17,
-              latitude: myPosition.latitude,
-              longitude: myPosition.longitude,
-            }}>
-            <Marker
-              coordinate={{
+        <ViewShot
+          ref={ref}
+          options={{format: 'jpg'}}
+          style={styles.mapViewContainer}>
+          <View style={styles.nmap}>
+            <NaverMapView
+              style={styles.nmap}
+              zoomControl={true}
+              center={{
+                zoom: 17,
                 latitude: myPosition.latitude,
                 longitude: myPosition.longitude,
-              }}
-              width={50}
-              height={50}
-              anchor={{x: 0.5, y: 0.5}}
-              caption={{text: '나'}}
-              image={require('@assets/logo.png')}
-            />
-            {path.length >= 2 ? (
-              <Polyline coordinates={path} strokeColor={MAIN_COLOR} />
-            ) : (
-              <></>
-            )}
-            {/* <Polyline coordinates={polylinePath} strokeColor={MAIN_COLOR} /> */}
-          </NaverMapView>
-        </View>
+              }}>
+              <Marker
+                coordinate={{
+                  latitude: myPosition.latitude,
+                  longitude: myPosition.longitude,
+                }}
+                width={50}
+                height={50}
+                anchor={{x: 0.5, y: 0.5}}
+                caption={{text: '나'}}
+                image={require('@assets/logo.png')}
+              />
+              {path.length >= 2 ? (
+                <Polyline coordinates={path} strokeColor={MAIN_COLOR} />
+              ) : (
+                <></>
+              )}
+              {/* <Polyline coordinates={polylinePath} strokeColor={MAIN_COLOR} /> */}
+            </NaverMapView>
+          </View>
+        </ViewShot>
         <Pressable
           onPress={() => {
             functions[0];
@@ -100,6 +175,7 @@ function LogViewTemplate({
     );
   }
 }
+
 const styles = StyleSheet.create({
   logViewContainer: {
     alignItems: 'center',
