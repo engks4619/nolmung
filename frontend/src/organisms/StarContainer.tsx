edba08270ee2fa84152553d9fe64958bf6c45264
engks4619 <engks4619@naver.com>
2022-11-16 06:24:30 +0900

    design: 산책 후 별점 별 이미지 변경 및 텍스트 제거

--- frontend/src/organisms/StarContainer.tsx
+++ frontend/src/organisms/StarContainer.tsx
@@ -1,11 +1,13 @@
 import React, {Dispatch, SetStateAction} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Rating} from 'react-native-ratings';
+import {Rating, AirbnbRating} from 'react-native-ratings';
 
 interface Props {
   star: number;
   setStar: Dispatch<SetStateAction<number>>;
 }
+
+const STAR_IMAGE = require('@assets/star.png');
 
 const StarContainer = ({star, setStar}: Props) => {
   return (
@@ -14,12 +16,24 @@ const StarContainer = ({star, setStar}: Props) => {
         <View>
           <Text style={styles.star}>{star} / 5 점</Text>
         </View>
+        {/* <AirbnbRating
+          defaultRating={5}
+          showRating={false}
+          // reviewColor={'rgb(241,196,15)'}
+          onFinishRating={(rating: number) => setStar(rating)}
+          starImage={STAR_IMAGE}
+        /> */}
         <Rating
+          type="custom"
           ratingCount={5}
+          ratingImage={STAR_IMAGE}
           jumpValue={0.1}
           onSwipeRating={(rating: number) => setStar(rating)}
           fractions={5}
           startingValue={5}
+          imageSize={40}
+          ratingBackgroundColor="#c8c7c8"
+          style={{marginHorizontal: 50}}
         />
       </View>
     </View>
