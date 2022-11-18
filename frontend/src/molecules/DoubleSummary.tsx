94fb304958e30b16b846476a45c99aeb0bdf144e
ryan8753 <ckdgus8753@gmail.com>
2022-11-18 13:36:42 +0900

    design:시간거리계산정수형으로

--- frontend/src/molecules/DoubleSummary.tsx
+++ frontend/src/molecules/DoubleSummary.tsx
@@ -14,15 +14,27 @@ function DoubleSummary({
   secondLabel,
   secondText,
 }: Props) {
+  const h = Math.floor(firstText / 3600);
+  const m = Math.floor((firstText % 3600) / 60);
+  const s = Math.floor((firstText % 3600) % 60);
+
+  const hDisplay = h > 0 ? h + (h === 1 ? ':' : ':') : '';
+  const mDisplay = m > 0 ? m + (m === 1 ? ':' : ':') : '';
+  const sDisplay = s > 0 ? s + (s === 1 ? '0' : '') : '';
+  const dist = Math.floor(secondText);
   return (
     <View style={styles.container}>
       <View style={[styles.infoContainer, styles.borderRight]}>
         <Text style={styles.fontSize}>{firstLabel}</Text>
-        <Text style={[styles.textCenter, styles.fontSize]}>{firstText}</Text>
+        <Text
+          style={[
+            styles.textCenter,
+            styles.fontSize,
+          ]}>{`${hDisplay}${mDisplay}${sDisplay}`}</Text>
       </View>
       <View style={[styles.infoContainer, styles.borderRight]}>
         <Text style={styles.fontSize}>{secondLabel}</Text>
-        <Text style={[styles.textCenter, styles.fontSize]}>{secondText}</Text>
+        <Text style={[styles.textCenter, styles.fontSize]}>{dist}</Text>
       </View>
     </View>
   );
