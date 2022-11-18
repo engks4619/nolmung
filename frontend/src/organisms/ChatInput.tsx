9e39661724a48b603b2ff7143ff56e6b672ce11f
j1ddong <hju191228@gmail.com>
2022-11-18 22:53:49 +0900

    feat: 채팅 헤더 설정

--- frontend/src/organisms/ChatInput.tsx
+++ frontend/src/organisms/ChatInput.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextInput, View, StyleSheet, Dimensions, Pressable} from 'react-native';
 import Send from '@assets/send.svg';
 
 const windowWidth = Dimensions.get('window').width;
 
 interface ChatInputProps {
-  msgInput: string;
-  onChageMsg: (text: string) => void;
   submitMsg: (text: string) => void;
 }
 
-function ChatInput({msgInput, onChageMsg, submitMsg}: ChatInputProps) {
+function ChatInput({submitMsg}: ChatInputProps) {
+  const [input, setInput] = useState<string>('');
+
+  const handleSubmit = (inputMsg: string) => {
+    submitMsg(inputMsg);
+    setInput('');
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder="메세지 보내기"
-        value={msgInput}
-        onChangeText={text => onChageMsg(text)}
-        onSubmitEditing={() => submitMsg(msgInput)}
+        value={input}
+        onChangeText={text => setInput(text)}
+        onSubmitEditing={() => handleSubmit(input)}
       />
       <Pressable
         style={styles.svgContainer}
-        onPress={() => submitMsg(msgInput)}>
+        onPress={() => handleSubmit(input)}>
         <Send width={25} height={25} fill={'black'} />
       </Pressable>
     </View>
