51801fb5357515b6f322855b6983f9b601a6d4e1
thwjd793@naver.com <thwjd793@naver.com>
2022-11-20 22:08:48 +0900

    feat: 게시글에 걸린 개정보

--- backend/community/src/main/java/com/a703/community/dto/response/connection/DogInfoDto.java
+++ backend/community/src/main/java/com/a703/community/dto/response/connection/DogInfoDto.java
@@ -2,9 +2,11 @@ package com.a703.community.dto.response.connection;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @JsonIgnoreProperties(ignoreUnknown =true)
 @Getter
 @NoArgsConstructor
@@ -16,6 +18,14 @@ public class DogInfoDto {
     private String dogName;
 
     private String image;
+
+    private Long dogIdx;
+
+    private Boolean neuter;
+
+    private Boolean vaccination;
+
+    private Character gender;
 
 
 }
